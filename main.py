f17acf2684451c490159d567eeb4ffa3e2fa1609
hanwool.lee <hanwool26@gmail.com>
2021-01-04 20:21:07 +0900

    second commit for github

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import dart_fss as dart
 
-api_key = 'cf795452c3cbfc9cd6f778baf04c4db18fd1acde'
+def api_open():
+    f = open('api_key.txt')
+    api_key = f.readline()
+    return api_key
+
+api_key = api_open()
+
 dart.set_api_key(api_key=api_key)
 
 crp_list = dart.get_corp_list()
 samsung = crp_list.find_by_corp_name('삼성전자', exactly=True)[0] #
 
-fs = samsung.extract_fs(bgn_de='20120101')
-
+fs = samsung.extract_fs(bgn_de='20200101')
 fs.save()
 
