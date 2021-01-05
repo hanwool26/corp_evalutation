5d5b555061f94c17d644698ec45be197d4617a79
hanwool.lee <hanwool26@gmail.com>
2021-01-05 17:57:52 +0900

    corp_info name of variable has been changed from samsung to corp_info

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ api_key = api_open()
 dart.set_api_key(api_key=api_key)
 
 crp_list = dart.get_corp_list()
-samsung = crp_list.find_by_corp_name('삼성전자', exactly=True)[0] #
+corp_info = crp_list.find_by_corp_name('삼성전자', exactly=True)[0] #삼성전자 재무제표
 
-fs = samsung.extract_fs(bgn_de='20200101')
+fs = corp_info.extract_fs(bgn_de='20200101')
 fs.save()
 
