e7bb11e1541f5bd3a77b3d2a6e7d2c1ff0f075f2
hanwool.lee <hanwool26@gmail.com>
2021-01-12 00:57:53 +0900

    adding finding corp_info funtion with title

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ def api_open():
     api_key = f.readline()
     return api_key
 
-api_key = api_open()
+def get_corp_info(corp_title="삼성전자"):
+    return crp_list.find_by_corp_name(corp_title, exactly=True)[0]
 
+api_key = api_open()
 dart.set_api_key(api_key=api_key)
 
-crp_list = dart.get_corp_list()
-corp_info = crp_list.find_by_corp_name('삼성전자', exactly=True)[0] #삼성전자 재무제표
-
+crp_list = dart.get_corp_list()  # Loading corp list
 
 if __name__ == '__main__':
-    print("main")
-    fs = corp_info.extract_fs(bgn_de='20200101')
-    fs.save()
+    print("기업명 :", end=' ')
+    corp_title = input()
+    corp_info = get_corp_info(corp_title)
+    if corp_info == None:
+        print(f'기업 없음{corp_title}')
+    else:
+        fs = corp_info.extract_fs(bgn_de='20200101')
+        fs.save()
 
 
 
