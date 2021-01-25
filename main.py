0fb78eb1007e25cdba20e5a2847b9be9b5c90b7e
hanwool.lee <hanwool26@gmail.com>
2021-01-26 00:14:36 +0900

    None type error return has been solved by getting rid of invalid index of list

--- main.py
+++ main.py
@@ -5,23 +5,21 @@ def api_open():
     api_key = f.readline()
     return api_key
 
-def get_corp_info(corp_title="삼성전자"):
-    return crp_list.find_by_corp_name(corp_title, exactly=True)[0]
-
-api_key = api_open()
-dart.set_api_key(api_key=api_key)
-
-crp_list = dart.get_corp_list()  # Loading corp list
+def get_corp_info(crp_list, corp_title="삼성전자"):
+    return crp_list.find_by_corp_name(corp_title, exactly=True)
 
 if __name__ == '__main__':
+    api_key = api_open()  # API KEY read 하여 string 할당
+    dart.set_api_key(api_key=api_key)
+
+    crp_list = dart.get_corp_list()  # Loading corp list
     print("기업명 :", end=' ')
     corp_title = input()
-    corp_info = get_corp_info(corp_title)
+    corp_info = get_corp_info(crp_list, corp_title)
     if corp_info == None:
         print(f'기업 없음{corp_title}')
     else:
-        fs = corp_info.extract_fs(bgn_de='20200101')
-        fs.save()
-
+        fs = corp_info[0].extract_fs(bgn_de='20200101')
+        print(type(fs))
 
 
